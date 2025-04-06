c00622c7567d0e69479cb42efeb9d9458963ba48
David Lins Do Amaral <linsdavid476@gmail.com>
2025-04-06 13:55:28 -0300

    Commit06041355

---
+++ ConceitosPOO/Data.java
@@ -0,0 +1,25 @@
+package ConceitosPOO;
+
+public class Data {
+
+    int dia;
+    int mes;
+    int ano;
+
+    Data(int recebeDia, int recebeMes, int recebeAno) {
+        dia = recebeDia;
+        mes = recebeMes;
+        ano = recebeAno;
+    }
+
+    Data() {
+        dia = 1;
+        mes = 1;
+        ano = 1970;
+    }
+
+    String juntarData () {
+        return String.format("%d/%d/%d", dia, mes, ano);
+    }
+
+}
---
+++ ConceitosPOO/dataTeste.java
@@ -0,0 +1,12 @@
+package ConceitosPOO;
+
+public class dataTeste {
+    public static void main(String[] args) {
+
+        Data d1 = new Data();
+        System.out.println(d1.juntarData());
+
+        Data d2 = new Data(13,07,2002);
+        System.out.println(d2.juntarData());
+    }
+}
