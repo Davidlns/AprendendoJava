842de4f9b6f54459aeb5f8e50267bb443523b3e6
David Lins Do Amaral <linsdavid476@gmail.com>
2025-04-09 11:54:52 -0300

    commit09042025

---
+++ TratamentoException/TratamentoEx.java
@@ -0,0 +1,17 @@
+package TratamentoException;
+
+public class TratamentoEx {
+    public static int divisao(int dividendo, int divisor) {
+        return dividendo / divisor;
+    }
+
+    public static void main(String[] args) {
+        try {
+            int resultado = divisao(10, 0);
+            System.out.println("Resultado: " + resultado);
+        } catch (ArithmeticException e) {
+            System.out.println("Erro: Divisão por zero!");
+        }
+    }
+    
+}
